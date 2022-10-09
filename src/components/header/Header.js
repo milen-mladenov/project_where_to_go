922506b1988a36619125f8fe2150fb499f81b647
Milen_Mladenov <milen_s_mladenov@yahoo.co.uk>
2022-10-09 19:17:31 +0300

    working on user profile page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,23 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+
 import logo from '../utility/svg/fork_knife_logo.svg'
 import styles from './Header.module.css'
-export const Header = () => {
 
-    const isLogged = false
+export const Header = () => {
+    const [isLogged, setIsLogged] = useState(true)
+
+    const name = "UserName"
+
+    function statusHandler() {
+        setIsLogged(!isLogged)
+    }
 
     return (
         <header>
             <Link to="/">
                 <div className={styles.logo_section}>
-                    <h3 className={styles.name}>WhereToGo</h3>
+                    <h2 className={styles.name}>WhereToGo</h2>
                     <img className={styles.logo} src={logo} />
                 </div>
             </Link>
@@ -19,8 +27,9 @@ export const Header = () => {
                     <Link to="/browse"><li>Browse</li></Link>
                     {isLogged ?
                         <div className={styles.login_section}>
+                            <h3>Welcome back {name}</h3>
                             <Link to="/profile"><li>Profile</li></Link>
-                            <Link to="/"><li>Logout</li></Link> </div> :
+                            <Link to="/" onClick={statusHandler}><li>Logout</li></Link> </div> :
                         <div className={styles.login_section}>
                             <Link to="/login"><li>Login</li></Link>
                             <Link to="/register"><li>Register</li></Link>
--- src/components/userProfilePage/UserProfilePage.js
+++ src/components/userProfilePage/UserProfilePage.js
@@ -1,5 +1,55 @@
-export const UserProfilePage = () =>{
-    return(
-        <h1>Profile</h1>
+import styles from './UserProfilePage.module.css'
+import pencil from '../utility/svg/redact_pencil.svg'
+
+export const UserProfilePage = () => {
+
+    const user = {
+        username: "Ivan Ivanov",
+        userImg: "",
+        userCountry: "Bulgaria",
+        email: "ivan_ivanov@abv.bg",
+        phone: "0888123456",
+        gender: "M",
+        visitedHistory: [
+            { location: "barOne", date: "12.11.2021", time: "15:30" },
+            { location: "barTwo", date: "01.6.2021", time: "21:30" },
+            { location: "barThree", date: "20.12.2021", time: "19:00" },
+            { location: "barOne", date: "25.07.2021", time: "12:15" }
+        ],
+        comments: []
+    }
+
+
+    return (
+        <section>
+            <h2>{user.username}</h2>
+            <div className={styles.user_img_section}>
+                <img src={user.userImg} />
+                <div>
+                    <p>Country:</p>
+                    <p>{user.userCountry}</p>
+                </div>
+                <div>
+                    <p>Gender:</p>
+                    <p>{user.gender}</p>
+                </div>
+            </div>
+            <div className={styles.details_section}>
+                <div className={styles.details_row}>
+                    <input type="email" value={user.email} disabled />
+                    <img className={styles.redact_pencil} src={pencil} />
+                </div>
+                <div className={styles.details_row}>
+                    <input type="number" value={user.phone} disabled />
+                    <img className={styles.redact_pencil} src={pencil} />
+                </div>
+                <div>
+                    <h3>Visited history:</h3>
+                    <ol>
+                        {user.visitedHistory.map(x => <li >You visited {x.location} on {x.date} at {x.time} </li>)}
+                    </ol>
+                </div>
+            </div>
+        </section>
     )
 }
