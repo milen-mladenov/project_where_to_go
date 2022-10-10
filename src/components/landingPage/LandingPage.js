8d9d68132aea5f2e17f09f775c5344e40212b304
Milen_Mladenov <milen_s_mladenov@yahoo.co.uk>
2022-10-10 22:19:29 +0300

    landing page css done

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -3,12 +3,11 @@ export const LandingPage = () => {
     return (
         <>
             <section className={styles.title}>
-                <h1>Don't know where to go today ?</h1>
+                <h1>Don't know where to go today?</h1>
                 <h2>You have come to the right place.</h2>
             </section>
 
             <section className={styles.information}>
-                <h2>Are you looking for a place to eat or just a drink?</h2>
                 <h3>Would you like to get a traditional cuisine?</h3>
                 <img src='https://justbulgaria.net/wp-content/uploads/2021/04/249_original-e1441724336118.jpg' />
                 <h3>Or just something to drink?</h3>
