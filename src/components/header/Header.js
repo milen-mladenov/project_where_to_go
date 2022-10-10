8f18d6a89c67b8077fb8cf9764aff4752df58e3e
Milen_Mladenov <milen_s_mladenov@yahoo.co.uk>
2022-10-10 18:11:34 +0300

    working on mobile css views

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,18 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
 import logo from '../utility/svg/fork_knife_logo.svg'
 import styles from './Header.module.css'
+import { naviMenu } from "../utility/images"
 
 export const Header = () => {
     const [isLogged, setIsLogged] = useState(true)
+    const [windowWidth, setWindowWidth] = useState(window.screen.width)
+    const [dropdown, setDropdown] = useState(false);
+
+    useEffect(() => {
+        setWindowWidth(window.screen.width)
+    }, [windowWidth])
 
     const name = "UserName"
 
@@ -13,30 +20,61 @@ export const Header = () => {
         setIsLogged(!isLogged)
     }
 
+    const mobileMenuIcon =
+        (<>
+            {dropdown ?
+                <div className={styles.dropdownSection} >
+                    <img className={styles.dropdownMenu} onClick={dropdownState} src={naviMenu.closeMenu} />
+                    <ul className={styles.dropdownOpened}>
+                        <Link to="/"><li>Home</li></Link>
+                        <Link to="/browse"><li>Browse</li></Link>
+                        {isLogged ?
+                            <div className={styles.login_section}>
+                                <Link to="/profile"><li>Profile</li></Link>
+                                <Link to="/" onClick={statusHandler}><li>Logout</li></Link> </div> :
+                            <div className={styles.login_section}>
+                                <Link to="/login"><li>Login</li></Link>
+                                <Link to="/register"><li>Register</li></Link>
+                            </div>
+                        }
+                    </ul>
+                </div> :
+                <div className={styles.dropdownSection}> <img className={styles.dropdownMenu} onClick={dropdownState} src={naviMenu.openMenu} /> </div>}
+        </>)
+
+    function dropdownState() {
+        setDropdown(!dropdown)
+    }
+
     return (
         <header>
-            <Link to="/">
-                <div className={styles.logo_section}>
-                    <h2 className={styles.name}>WhereToGo</h2>
-                    <img className={styles.logo} src={logo} />
-                </div>
-            </Link>
-            <nav className={styles.navigation}>
-                <ul>
-                    <Link to="/"><li>Home</li></Link>
-                    <Link to="/browse"><li>Browse</li></Link>
-                    {isLogged ?
-                        <div className={styles.login_section}>
-                            <h3>Welcome back {name}</h3>
-                            <Link to="/profile"><li>Profile</li></Link>
-                            <Link to="/" onClick={statusHandler}><li>Logout</li></Link> </div> :
-                        <div className={styles.login_section}>
-                            <Link to="/login"><li>Login</li></Link>
-                            <Link to="/register"><li>Register</li></Link>
-                        </div>
-                    }
-                </ul>
-            </nav>
+            <div className={styles.header_content}>
+                <Link to="/">
+                    <div className={styles.logo_section}>
+                        {windowWidth < 400 ? <h2 className={styles.name}>WTG</h2> : <h2 className={styles.name}>WhereToGo</h2>}
+                        <img className={styles.logo} src={logo} />
+                    </div>
+                </Link>
+                {windowWidth < 900
+                    ? mobileMenuIcon
+                    : <nav className={styles.navigation}>
+                        <ul>
+                            <Link to="/"><li>Home</li></Link>
+                            <Link to="/browse"><li>Browse</li></Link>
+                            {isLogged ?
+                                <div className={styles.login_section}>
+                                    <h3>Welcome back {name}</h3>
+                                    <Link to="/profile"><li>Profile</li></Link>
+                                    <Link to="/" onClick={statusHandler}><li>Logout</li></Link> </div> :
+                                <div className={styles.login_section}>
+                                    <Link to="/login"><li>Login</li></Link>
+                                    <Link to="/register"><li>Register</li></Link>
+                                </div>
+                            }
+                        </ul>
+                    </nav>}
+
+            </div>
         </header>
     )
 }
---
+++ src/components/utility/images.js
@@ -0,0 +1,7 @@
+import openMenu from './svg/openMenu.svg'
+import closeMenu from './svg/closeMenu.svg'
+
+export const naviMenu ={
+    openMenu,
+    closeMenu
+}
